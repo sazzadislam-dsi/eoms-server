b63f6c9204d59a224ee33c923972799f9e6e8ce9
lynas <szlynas@gmail.com>
2017-03-18 22:41:42 +0600

    main class in kotlin

---
+++ src/main/kotlin/com/lynas/Application.kt
@@ -0,0 +1,27 @@
+package com.lynas
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * Created by lynas on 3/18/2017
+ */
+
+@SpringBootApplication
+open class Application
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
+
+
+
+
+
+
+
+
+
+
+
