1c382eb516a2c6c89b34b474d421ef1121745bd3
seal <hasanat.muztaba@gmail.com>
2017-06-17 16:32:18 +0600

    add nullable [Fixed NPE in studentDetails page when redirect after creating new Student]

--- src/main/kotlin/com/lynas/model/response/ExamClassResponse1.kt
+++ src/main/kotlin/com/lynas/model/response/ExamClassResponse1.kt
@@ -7,10 +7,10 @@ import com.lynas.model.util.ExamType
  * Created by seal on 5/26/2017.
  */
 class ExamClassResponse1 {
-    var studentId = 0L
-    var classId = 0L
-    var rollNumber = 0
-    var year = 0
+    var studentId: Long? = 0L
+    var classId: Long? = 0L
+    var rollNumber: Int? = 0
+    var year: Int? = 0
 
     constructor(studentId: Long, classId: Long, rollNumber: Int, year: Int, grandTotal: GrandTotal, subjects: List<Subjects>) {
         this.studentId = studentId
@@ -22,17 +22,17 @@ class ExamClassResponse1 {
     }
 
     data class GrandTotal(
-            var total: Double = 0.0,
-            var obtained: Double = 0.0
+            var total: Double? = 0.0,
+            var obtained: Double? = 0.0
     )
 
     var grandTotal: GrandTotal = GrandTotal()
 
     data class Subjects (
-            var subjectName: String = "N/A",
-            var total: Double = 0.0,
-            var obtained: Double = 0.0,
-            var exams: Map<ExamType, Exam> = mutableMapOf()
+            var subjectName: String? = "N/A",
+            var total: Double? = 0.0,
+            var obtained: Double? = 0.0,
+            var exams: Map<ExamType, Exam>? = mutableMapOf()
     )
 
     var subjects: List<Subjects> = mutableListOf()
