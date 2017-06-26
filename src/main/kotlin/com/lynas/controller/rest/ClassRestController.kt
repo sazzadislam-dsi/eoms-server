4745cef5a1e8c860b969f85ac871c16dbb26c203
seal <hasanat.muztaba@gmail.com>
2017-06-27 04:15:05 +0600

    check duplicate class using exception

--- src/main/kotlin/com/lynas/controller/rest/ClassRestController.kt
+++ src/main/kotlin/com/lynas/controller/rest/ClassRestController.kt
@@ -1,5 +1,6 @@
 package com.lynas.controller.rest
 
+import com.lynas.exception.DuplicateCourseException
 import com.lynas.model.Course
 import com.lynas.service.ClassService
 import com.lynas.util.getOrganizationFromSession
@@ -7,6 +8,7 @@ import com.lynas.util.responseConflict
 import com.lynas.util.responseOK
 import com.lynas.util.verifyClassOrganization
 import org.slf4j.LoggerFactory
+import org.springframework.dao.DuplicateKeyException
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
@@ -28,10 +30,16 @@ open class ClassRestController (val classService: ClassService) {
         logger.info("Received Class :: " + cls.toString())
         var createdClass = cls
         createdClass.organization = getOrganizationFromSession(request)
-        if (classService.checkClassAlreadyExist(cls, getOrganizationFromSession(request).id!!)) {
+
+        try {
+            createdClass = classService.save(createdClass)
+        }
+        catch (ex: DuplicateCourseException) {
+            logger.warn("Duplicate class info found, class name [{}], shift [{}], section [{}]", cls.name, cls.shift, cls.section)
+            return responseConflict(cls)
+        }catch (ex: DuplicateKeyException){
             return responseConflict(cls)
         }
-        createdClass = classService.save(createdClass)
         logger.info("Saved Class :: " + createdClass.toString())
         return responseOK(createdClass)
     }
