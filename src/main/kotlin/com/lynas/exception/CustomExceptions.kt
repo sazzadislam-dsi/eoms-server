202b5d7fed6fc52098bbf0aeb6042d6bd4f1df91
LynAs <szlynas@gmail.com>
2017-12-02 23:14:52 +0600

    refactor

--- src/main/kotlin/com/lynas/exception/CustomExceptions.kt
+++ src/main/kotlin/com/lynas/exception/CustomExceptions.kt
@@ -4,15 +4,17 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 import java.text.ParseException
 
-
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 class DuplicateEntryException(message: String) : IllegalStateException(message)
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 class EntityNotFoundException(message: String) : NullPointerException(message)
 
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 class DateFormatParseException(message: String, errorOffset: Int): ParseException(message, errorOffset)
 
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 class ConstraintsViolationException(message: String) : Exception(message)
-
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 class UnauthorizedAccessException(message: String) : Exception(message)
