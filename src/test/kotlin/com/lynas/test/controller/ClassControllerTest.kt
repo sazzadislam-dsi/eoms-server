2fe9c254db6de5806bf4e76d29acef37cc42e35e
sazzad <sazzad.islam@dsinnovators.com>
2018-02-12 17:46:02 +0600

    FIRST test added

--- src/test/kotlin/com/lynas/test/controller/ClassControllerTest.kt
+++ src/test/kotlin/com/lynas/test/controller/ClassControllerTest.kt
@@ -1,13 +1,14 @@
 package com.lynas.test.controller
 
-import com.lynas.dto.AuthenticationRequestDTO
-import com.lynas.dto.AuthenticationResponseDTO
+import com.lynas.test.authToken
 import com.lynas.util.getLogger
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.devtools.remote.client.HttpHeaderInterceptor
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
@@ -15,20 +16,28 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class AuthenticationControllerTest {
+class ClassControllerTest {
 
-    private val log = getLogger(AuthenticationControllerTest::class.java)
+    private val log = getLogger(ClassControllerTest::class.java)
     @Autowired
     lateinit var testRestTemplate: TestRestTemplate
 
+    @Before
+    fun init() {
+        testRestTemplate.restTemplate.interceptors = listOf(HttpHeaderInterceptor("Authorization", authToken))
+    }
+
 
     @Test
-    fun testLogin() {
-        val authDto = AuthenticationRequestDTO("admin", "123456")
-        val result = testRestTemplate.postForEntity("/auth/login", authDto, AuthenticationResponseDTO::class.java)
-        log.info("$$$" + result.body.token)
+    fun testHelloController() {
+        val result = testRestTemplate.getForEntity("/classes", List::class.java)
         assertEquals(result.statusCode, HttpStatus.OK)
         assertNotNull(result.statusCodeValue)
+        result.body.forEach {
+            if (it is LinkedHashMap<*, *>) {
+                log.info("$$$ ${it["id"]}")
+            }
+        }
     }
 
 }
